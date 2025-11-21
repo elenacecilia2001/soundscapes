2b239dc2d94648f80c65343955ad46c2f4af391d
elena <elena.cecilia2001@gmail.com>
2025-11-21 15:20:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById("closePopup").addEventListener("click", () => {
-  // Invece di nasconderlo bruscamente con "none", 
-  // aggiungiamo la classe che fa partire la dissolvenza CSS
   document.getElementById("popup").classList.add("hidden");
 });
+
